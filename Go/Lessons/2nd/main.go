5e29415852953ad886d120a2980cbf921b446fbc
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-09-10 16:18:21 +0500

    Create main.go

---
+++ Go/Lessons/2nd/main.go
@@ -0,0 +1,39 @@
+package main	
+
+//Data Types
+// 
+// (Numeric)--------------------------------------------------------------------------
+// 
+// 		Int - integer
+// 
+//  			int8 = -128....127
+// 	signed		int16 = -32,768....32767
+// 				int32 = -2,147,483,648....2,147,483,647
+// 				int64 = (very big number)
+// 
+// 				unit8 = 0....255
+// 	unsigned	unit16 = 0....65,535
+// 				unit32 = 0....4,294,967,296
+// 				uint64 = (very big number)
+//     -------------------------------------------------------------------------------
+// 		Float - floating point numbers
+// 
+// 				float32 - (very big value)
+// 				float64 - (very big value)
+// 
+// (Boolean)--------------------------------------------------------------------------
+// 
+// 		Boolean - boolean value by default (false)
+// 				
+// 				true
+// 				false
+// 
+// (String)---------------------------------------------------------------------------
+// 
+// 		String - string value
+// 				
+// 				string byte = unit8 (for printing characters from ASCII)
+// 				string rune = unit32 (for printing characters from UTF-8)
+// 
+// -----------------------------------------------------------------------------------
+
