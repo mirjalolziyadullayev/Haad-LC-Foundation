b190f56c60f827cea0db13fd99dacb29ea3b8b21
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-09-28 22:27:09 +0500

    Create main.go

---
+++ Go/Lessons/Lesson tasks/80 task/decimal to others/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println(get(10, 10))
+}
+
+func get(decimal int, base int) (res string) {
+  letters := "0123456789abcdefghijklmnopqrstuwxyz"
+
+  for decimal > 0 {
+
+    res = string(letters[decimal%base]) + res
+    decimal = decimal / base
+  }
+  return res
+}
